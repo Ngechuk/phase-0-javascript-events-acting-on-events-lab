d4006ee7ea22b56521bd26ccc3e72b771e8073cd
Kevin <kelvinngechu@gmail.com>
2022-11-25 18:25:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code here
+const moveDodgerLeft = () => {
+    let dodger = document.querySelector('#dodger');
+    dodger.style.left = 0;
+  };
+  
+  const moveDodgerRight = () => {
+    let dodger = document.querySelector('#dodger');
+    dodger.style.left = "180px";
+    dodger.style.right = 0;
+  }
+ 
